7e80f1fa3cdb5bdb146a3430f5b6509883cd69b9
jakhussain <jakir.net@gmail.com>
2013-07-26 11:14:59 +0530

    Smaple project to test bubble parent issue
    Smaple project to test bubble parent issue with Titanium SDK 3.1.0

---
+++ app/controllers/index.js
@@ -0,0 +1,15 @@
+function doClick(e) {  
+    alert($.label.text);
+}
+
+function tblView_click(e)
+{
+	alert('table row clicked: ' + e.row.id)
+}
+
+function ButtonClick()
+{
+	alert('Button Clicked !!!');
+}
+
+$.index.open();
